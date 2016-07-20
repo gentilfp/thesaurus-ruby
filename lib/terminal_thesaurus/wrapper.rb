27e9f56b244cf10e304ec6185726f4e374e99f37
Felipe Gentil <cdigentil@gmail.com>
2016-07-20 13:00:25 -0300

    Ugly parser for synonyms and antonyms

--- lib/terminal_thesaurus/wrapper.rb
+++ lib/terminal_thesaurus/wrapper.rb
@@ -1,13 +1,40 @@
+require 'open-uri'
+require 'nokogiri'
+
 module TerminalThesaurus
   class Wrapper
-    attr_reader :word, :synonyms, :antonyms, :related
+    attr_reader :word, :url, :synonyms, :antonyms, :related
 
     def initialize(word)
       @word = word
-      @synonyms, @antonyms, @related = nil
+      @url = "http://www.thesaurus.com/browse/#{word}?s=t"
+      @synonyms, @antonyms, @related = [], [], []
     end
 
     def look_up
+      parse(Nokogiri::HTML(open(url)))
+    rescue OpenURI::HTTPError
+      "Cannot connect to thesaurus.com"
+    end
+
+    private
+    def parse(page)
+      # synonyms
+      page.css('.synonyms .relevancy-list ul').each do |column|
+        column.css('li').each do |row|
+          @synonyms << row.children[0].children[0].children[0].text
+        end
+      end
+
+      # antonyms
+      page.css('.antonyms .list-holder .list').each do |column|
+        column.css('li').each do |row|
+          @antonyms << row.children[0].children[0].children[0].text
+        end
+      end
+
+      # related
+      nil
     end
   end
 end
--- spec/terminal_thesaurus/wrapper_spec.rb
+++ spec/terminal_thesaurus/wrapper_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe TerminalThesaurus::Wrapper do
     subject { described_class.new(word) }
     let(:word) { 'after' }
 
-    it 'initializes with nil values' do
+    it 'initializes with empty values' do
       [:synonyms, :antonyms, :related].each do |attribute|
-        expect(subject.send(attribute)).to be_nil
+        expect(subject.send(attribute)).to be_empty
       end
     end
 
@@ -17,6 +17,23 @@ RSpec.describe TerminalThesaurus::Wrapper do
   end
 
   describe '#look_up' do
+    subject { described_class.new('beautiful') }
+    let(:url) { "http://www.thesaurus.com/browse/beautiful?s=t" }
+    after { subject.look_up }
+
+    context 'success' do
+      it 'parses synonyms and antonyms' do
+        stub_request(:any, url).to_return(body: 'nokogiri')
+        expect(subject).to receive(:parse).with(anything)
+      end
+    end
+
+    context 'failure' do
+      it 'catches exception and returns a message' do
+        stub_request(:any, url).to_raise(OpenURI::HTTPError.new('', nil))
+        expect(subject).to_not receive(:parse)
+      end
+    end
   end
 
 end
