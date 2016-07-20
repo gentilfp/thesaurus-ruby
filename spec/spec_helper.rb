12861643ca3c6d0f87cae0a8ed0d6c29320ba4bc
Felipe Gentil <cdigentil@gmail.com>
2016-07-20 12:04:26 -0300

    Add dependencies and setup spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,30 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
 require 'terminal_thesaurus'
+require 'webmock/rspec'
+
+RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+end
--- terminal_thesaurus.gemspec
+++ terminal_thesaurus.gemspec
@@ -27,7 +27,12 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "nokogiri", "~> 1.6.3"
+
   spec.add_development_dependency "bundler", "~> 1.12"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "simplecov", "~> 0.9.0"
+  spec.add_development_dependency "webmock", "~> 1.19"
+  spec.add_development_dependency "byebug"
 end
